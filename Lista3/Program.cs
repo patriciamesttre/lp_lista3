3ba001ad9d7256ccfbb6fd9dfce880e5759a85b8
patriciamesttre <patriciamesttre@hotmail.com>
2018-11-23 22:29:04 -0200

    Resolvi o primeiro exercicio

--- Lista3/Program.cs
+++ Lista3/Program.cs
@@ -6,7 +6,24 @@ namespace Lista3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           /*Escreva um programa que armazene o valor 999 na variável A e
+           o valor 555 na variável B. A seguir troque os conteúdos  
+           fazendo com que o valor que está em A passe para B e
+           vice-versa utilizando uma variável C para realizar a troca. */
+         
+           int A = 999;
+           int B = 555;
+           int C = 0;
+           Console.WriteLine("A ==> " + A );
+           Console.WriteLine("B ==> " + B );
+           Console.WriteLine("C ==> " + C );
+           Console.WriteLine("== TROCA == " );
+           C = A;
+           A = B;
+           B = C;
+           Console.WriteLine("A ==> " + A );
+           Console.WriteLine("B ==> " + B );
+           Console.WriteLine("C ==> " + C );
         }
     }
 }
